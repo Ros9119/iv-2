3e1079c8a6e0046ca4da4a8543e4771efff7f53f
Rosario <rosariogrieco89@gmail .com>
2023-04-17 14:43:45 +0200

    Initial commit

---
+++ src/test/java/org/example/RectangleTest.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RectangleTest {
+
+    Rectangle rectangle;
+    @BeforeEach
+    void setUp() {
+        Rectangle rectangle = new Rectangle(5,10);
+    }
+
+    @Test
+    void testGetLength() {
+        assertEquals(5,rectangle.getLength());
+    }
+
+    @Test
+    void testGetWidth() {
+        assertEquals(10,rectangle.getWidth());
+    }
+
+    @Test
+    void testGetArea() {
+        assertEquals(50,rectangle.getArea());
+    }
+
+    @Test
+    void testGetPerimeter() {
+        assertEquals(30,rectangle.getPerimeter());
+    }
+}
